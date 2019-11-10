e0c12f041285395be304edaf4dbd8ecf75f09b90
shubham <shubham@marmeto.com>
2019-11-11 01:12:11 +0530

    added edit commit, condition and reply comment

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -4,7 +4,7 @@ let User = require('../models/user')
 //render comment page
 exports.showCommentPage = (req, res) => {
     let user_id = req.params.userId;
-    console.log("cookies user_sid - "+req.cookies.user_sid);
+    console.log("cookies user_sid - " + req.cookies.user_sid);
     if (req.cookies.user_sid != "logout") {
         User.find({ _id: user_id }).then((result) => {
             // console.log(result);
@@ -69,7 +69,32 @@ exports.getreply = (req, res) => {
 
 //
 exports.postReply = (req, res) => {
+    let text = req.body.text;
+    let commentId = req.body.id;
+    let userName = req.body.name;
 
+    //console.log(text + "             " + commentId + "            " + userName);
+
+    mysqlConnection.query("INSERT INTO reply (id, comment_id, text, isDeleted, userName) VALUES ( NULL, '" + commentId + "','" + text + "', '1', '" + userName + "')", (err, rows) => {
+        if (!err) {
+            if (rows) {
+                res.send({
+                    status: true
+                });
+            }
+            else {
+                res.send({
+                    status: false
+                });
+            }
+        }
+        else {
+            console.log(err);
+            res.send({
+                status: false
+            });
+        }
+    });
 }
 
 //
@@ -77,9 +102,9 @@ exports.editComment = (req, res) => {
     let text = req.body.text;
     let id = req.body.id;
 
-    console.log(text+"              "+id);
+    //console.log(text + "              " + id);
 
-    mysqlConnection.query("INSERT INTO comment (id, text, dateTime, isDeleted, userId) VALUES ( NULL,'" + text + "','2019-11-10' , '1', '" + id + "')", (err, rows) => {
+    mysqlConnection.query("UPDATE comment SET text='" + text + "' WHERE id=" + id, (err, rows) => {
         if (!err) {
             if (rows) {
                 res.send({
--- routes/comment.js
+++ routes/comment.js
@@ -7,9 +7,9 @@ router.get('/all',comment.getAllComment);
 router.get('/:userId',comment.showCommentPage);
 router.post('/create',comment.createComment);
 router.put('/edit',comment.editComment);
+router.post('/reply',comment.postReply);
 
 router.get('/reply/:comment_id',comment.getreply);//not done
-router.post('/reply/:comment_id',comment.postReply);//not done
 
 
 //two more api
